9328d20c7b2cfa8abfb6f8676fab1f72a2f71f08
paulop <jan.voss@outlook.pt>
2019-11-08 11:56:35 -0600

    added basic navigation and basic layout for sub activities

--- app/src/main/java/com/example/animationstransitions/PropertyAnimActivity.kt
+++ app/src/main/java/com/example/animationstransitions/PropertyAnimActivity.kt
@@ -1,12 +1,19 @@
 package com.example.animationstransitions
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class PropertyAnimActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_property_anim)
+    }
+
+    fun backBtnClicked(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
